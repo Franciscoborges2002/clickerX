40c9beee73ce39398d1b8275aa02119859c5ca02
Francisco Borges <franciscomsborges2002@gmail.com>
2023-01-23 03:26:09 +0000

    Save added, save and load

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let balanceDisplay = document.getElementById("balanceDisplay");
 
 const clickPowerPrices = [{1: 50}, {5: 1000}]//Array for the prices of the clickpower
 const autoPrices = [{1: 1500}, {5: 5000}]//Array for the prices of the clickpower
-var balance = 0;//Change when we add the browser load
-var clickPower = 1;
-var auto = 0;
+var balance;
+var clickPower;
+var autoPower;
 
 function addToBalance(){
     balance += clickPower;
@@ -32,7 +32,7 @@ function upgradeAuto(index, value2Upgrade){
     if(balance < autoPrices[index][value2Upgrade]){//Verify if has balance
         alert("Not enough balance");
     }else{
-        auto += value2Upgrade;//Add to the clickPower
+        autoPower += value2Upgrade;//Add to the clickPower
         balance -= autoPrices[index][value2Upgrade];//Remove value from balance
         autoPrices[index][value2Upgrade] = autoPrices[index][value2Upgrade] * 1.3;//Update value
     }
@@ -41,8 +41,10 @@ function upgradeAuto(index, value2Upgrade){
 }
 
 /*ADD TO AUTO BALANCE*/
-setInterval(() =>{
-    balance += auto;
+setInterval(() =>{//use anonymous function
+    balance = balance + autoPower;
+    console.log("autoPower " + autoPower)
+    console.log("balance " + balance)
 }, 1000);
 
 /*UPDATE PART*/
@@ -76,4 +78,40 @@ function updateDiplayAutoCostsClick(updateNumber, value2Upgrade){
         default:
             break;
     }
+}
+
+/* SAVE & LOAD GAME PART */
+
+function loadGame(){
+    console.log('LOADING GAME FROM LOCAL STORAGE');
+
+    if(localStorage.getItem('clickPower') === null){//If has clickPower is different than null, it has some value
+        balance = 0;
+        clickPower = 1;
+        autoPower = 0;
+    }else{//Start the values from 0
+        balance = localStorage.getItem('balance');
+        clickPower = localStorage.getItem('clickPower');
+        autoPower = localStorage.getItem('autoPower');
+    }
+    
+    console.log("autoPower " + autoPower)
+}
+
+function saveGame(){
+    console.log("SAVING GAME TO LOCAL STORAGE");
+    if(clickPower === "null"){
+        alert("Please make some progress in the game before saving the game.");
+    }else{
+        localStorage.setItem('balance', balance);
+        localStorage.setItem('clickPower', clickPower);
+        localStorage.setItem('autoPower', autoPower);
+    }
+}
+
+function deleteGame(){
+    if (confirm("Do you really want to delete the game?") == true) {
+        localStorage.clear();
+        window.location.reload();
+      }
 }
